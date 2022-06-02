76142b76fdf57404388c47e0f4f1288fec70928d
fay-muema <faithmuema45@gmail.com>
2022-06-02 15:45:23 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the codes starts here
+ *
+ * Descripition - displays the out put of the string
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
