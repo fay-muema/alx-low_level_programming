27674282af83128260602592670a6417bde17f32
fay-muema <faithmuema45@gmail.com>
2022-06-02 09:05:44 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#!/bin/bash
+
 #include <stdio.h>
 #include <string.h>
-int main() {
+int main(void) {
 	puts("Programming is like building a multilingual puzzle\n");
-		retutn (0);
+	 return (0);
 }
 
