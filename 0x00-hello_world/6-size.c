f88f30a641b781e881716457745907e8ec535951
fay-muema <faithmuema45@gmail.com>
2022-06-02 13:14:28 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	float d;
 	char e;
 
-      	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(e));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
-        return (0);
+	return (0);
 }
