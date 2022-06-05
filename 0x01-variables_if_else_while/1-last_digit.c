ee99e3d065f9007d08804e8e758239125e6b137e
fay-muema <faithmuema45@gmail.com>
2022-06-05 11:10:37 -0700

    digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -34,7 +34,7 @@ int main(void)
 				printf("Last digit of %d is ", n);
 				if (n > 5)
 				{
-					printf("%d and is 00\n", lastDigit);
+					printf("%d and is 0\n", lastDigit);
 				}
 				else if (n == 0)
 				{
