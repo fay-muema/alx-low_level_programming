8123df720196d9ab9bfc3866204fb86fa0c833ef
fay-muema <faithmuema45@gmail.com>
2022-06-06 12:22:16 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - the code starts here
+ *
+ * Description - the main code goes here
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
