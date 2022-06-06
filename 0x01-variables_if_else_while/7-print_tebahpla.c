22385522e4e98c7fcb0a649a7538d6b12f51b0a1
fay-muema <faithmuema45@gmail.com>
2022-06-06 12:40:23 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - where the code starts
+ *
+ * Description - the main code ges here
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch--)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
