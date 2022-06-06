b55cf5a87f5df866ce2817236b19fbb7eaab2874
fay-muema <faithmuema45@gmail.com>
2022-06-06 10:08:56 -0700

    alpha upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#include <ctype.h>
+
+/**
+ * main - this is where the code begins
+ *
+ * Description - this is where the code goes
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		int to_upper = toupper(ch);
+
+		putchar(ch);
+		
+		putchar('\n');
+
+		putchar(to_upper);
+
+	}
+
+	return (0);
+
+
+
+}
