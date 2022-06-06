7af574650e562ca642949ad45ca75f676e0d4bae
fay-muema <faithmuema45@gmail.com>
2022-06-06 10:39:03 -0700

    printing two numbers exept 2

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - the code starts here
+ *
+ * Description - the main code goes here
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+}
