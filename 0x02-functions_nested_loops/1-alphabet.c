b86464ce8415e1fb80bdff3cfa33850c25be0cb2
fay-muema <faithmuema45@gmail.com>
2022-06-07 13:06:08 -0700

    all letters
    Signed-off-by: fay-muema <faithmuema45@gmail.com>

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the code starts here
+ *
+ * Description - the main code goes here where the question is answered
+ *
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
