bb7f8bcb8c4bbab902bbc2a3a592d4ed831afad2
fay-muema <faithmuema45@gmail.com>
2022-06-07 10:31:59 -0700

    intro to loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the code starts here
+ *
+ * Description - the code output comes
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
