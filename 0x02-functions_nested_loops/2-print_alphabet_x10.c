2bf5676e8cd1a3b1d8b9a46f3a816e4f3e5858d3
fay-muema <faithmuema45@gmail.com>
2022-06-09 04:18:39 -0700

    lowerr case alphabets 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,9 +11,9 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (i = '1'; i <= '9'; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
