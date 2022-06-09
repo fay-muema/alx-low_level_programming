b9abcbceace86844c48c54f5f246cf024162d4df
fay-muema <faithmuema45@gmail.com>
2022-06-09 04:03:36 -0700

    numbers 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,10 +13,11 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i <= '9'; i++)
 	{
-		for (j = 'a'; j <= 'b'; j++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
-		_putchar('\n');
+		
 	}
+	_putchar('\n');
 }
