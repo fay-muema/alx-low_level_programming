2cc5e9c5f6eff67df5aef1e6bf78427f0f5e1317
fay-muema <faithmuema45@gmail.com>
2022-06-09 05:41:04 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - checks whether is lower
+ *
+ * Return: returns 0 for uppercase and returns 1 for lowercase
+ */
+
+int _islower(int c)
+{
+
+	if (c == 'c')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,9 +6,11 @@
 
 int _putchar(char);
 
-void print_alphabet(void);
+void print_alphaibet(void);
 
 void print_alphabet_x10(void);
+
+int _islower(int c);
 
 
 #endif
