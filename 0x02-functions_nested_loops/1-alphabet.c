a3d75cee12679ab6a9ac62802ac64c15f53a38c5
fay-muema <faithmuema45@gmail.com>
2022-06-09 03:03:15 -0700

    messagge

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all the alphabets in lowercase
+ *
+ * Return: always returns o
+ */
+
+void print_alphabet(void)
+{
+	int b;
+
+	for(b = 'a'; b <= 'z'; b++)
+	{
+		_putchar(b);
+	}
+	_putchar('\n');
+}
