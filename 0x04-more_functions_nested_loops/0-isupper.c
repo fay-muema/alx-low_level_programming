f38b6ca6fdb42bc13c45e13b255d5b4804487bf7
fay-muema <faithmuema45@gmail.com>
2022-06-10 14:06:58 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - prints if it is upper or if it is lower
+ * @c: input character
+ * Return: returns o
+ */
+
+int _isupper(int c)
+{
+	if (c == 'A' || c == 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#define _MAIN_H_
+#ifndef _MAIN_H_
+
+int _isupper(int c);
+
+#endif
