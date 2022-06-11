b06ed0d3cbdbe699fb73693d508d45421c7ce71a
fay-muema <faithmuema45@gmail.com>
2022-06-11 02:23:42 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - is a function name
+ * @a: the character is initialized to it
+ * @b: initialized to that
+ * Return: returns 0
+ */
+
+int mul(int a, int b)
+{
+	a * b;
+}
