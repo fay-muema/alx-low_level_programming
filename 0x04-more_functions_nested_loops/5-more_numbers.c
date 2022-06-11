add94ca1eaa146357951eb9c1b000023f073478e
fay-muema <faithmuema45@gmail.com>
2022-06-11 12:42:49 -0700

    dogits

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,12 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 14; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			_putchar(j);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
