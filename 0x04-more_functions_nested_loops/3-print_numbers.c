d951ea6d5da4fca708f20dde6dc85e8e862f0591
fay-muema <faithmuema45@gmail.com>
2022-06-11 02:46:54 -0700

    putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - this is where the numbers will be printed
+ * Return: returns 0
+ */
+
+void print_numbers(void)
+{
+	int i
+		;
+	for (i >= 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
