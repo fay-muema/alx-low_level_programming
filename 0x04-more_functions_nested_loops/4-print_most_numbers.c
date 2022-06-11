842a6e3bee47a9485fc54bd06b6b5b2af1c42bb1
fay-muema <faithmuema45@gmail.com>
2022-06-11 08:43:34 -0700

    most digits

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
+/**
+ * print_most_numbers - prints all the numbers except 4 and 2
+ * Return: returns 0
+ */
+
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 9; i++)
 	{
 		if (i != 2 && i != 4)
 		{
