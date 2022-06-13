4c92f5b77724f8a94735015783ad9fa1194e343d
fay-muema <faithmuema45@gmail.com>
2022-06-13 10:49:15 -0700

    swaped numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - where the swap happens
+ * @a: integer intialized to it
+ * @b: initialized to another integer
+ * Return: returns 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	a = b;
+	b = a;
+}
