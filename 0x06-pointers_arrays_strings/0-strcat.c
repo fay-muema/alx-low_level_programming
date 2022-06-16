bac447a30dc7eab7c1fd2d3616938384df4da261
fay-muema <faithmuema45@gmail.com>
2022-06-16 09:34:33 -0700

    strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates the two strings
+ * @dest: the detination.
+ * @src: the source
+ * Return: return the pointer to destination
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	while (*(dest + a) != '\0')
+	{
+		a++;
+	}
+
+	while (b >= 0)
+	{
+		*(dest + a) = *(src + b);
+		if (*(src + b) == '\0')
+			break;
+		a++;
+		b++;
+	}
+return (dest);	
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+har *_strcat(char *dest, char *src);
+#endif
