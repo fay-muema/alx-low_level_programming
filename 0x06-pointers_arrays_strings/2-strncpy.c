eca8749fcc20bcf531a0fdc490886a68944d2df8
fay-muema <faithmuema45@gmail.com>
2022-06-16 10:17:48 -0700

    this is good

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: is the destination of the file
+ * src: the source.
+ * @n: the number of bytes from src.
+ * Return: points to the destination
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[a] = src[a];
+	for (a = 0; a < n; a++)
+		dest[a] = '\0';
+	return (dest);
+}
