2379188c05171e152a90b2e6825647becca1df7e
fay-muema <faithmuema45@gmail.com>
2022-06-16 14:46:45 -0700

    strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the arrays
+ * @a: array
+ * @n: number of elements of the array
+ * Return: no return
+ */
+
+
+void reverse_array(int *a, int n)
+{
+	int b;
+	int c;
+	int d;
+
+	{
+		for (b = 0; b < n - 1; b++)
+		{
+			for (c = b + 1; c > 0; c--)
+			{
+				d = *(a + c);
+				*(a + b) = *(a + (c - 1));
+				*(a + (b - 1)) = d;
+			}
+		}
+	}
+}
