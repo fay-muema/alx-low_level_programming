d91942981a8ee65c1b8e778620521880bf419c9a
fay-muema <faithmuema45@gmail.com>
2022-06-16 15:29:47 -0700

    string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * cap_string - makes all words of a string capital
+ * @k: input string
+ * Return: returns the pointer
+ */
+
+
+char *cap_string(char *k)
+{
+	int count = 0;
+       int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};	
+
+}
+
