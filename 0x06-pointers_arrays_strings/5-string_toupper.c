bb3c2bf0f8d5df753fe6fcede4e1b42a20b8877f
fay-muema <faithmuema45@gmail.com>
2022-06-16 15:15:03 -0700

    string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string
+ * @s: input string
+ * Return: returns to desitanatiom
+ */
+
+char *string_toupper(char *s)
+{
+	int a = 0;
+
+	while (*(s + a) != '\0')
+	{
+		if ((*(s + a) >= 97) && (*(s + a) <= 122))
+			*(s + a) = *(s + a) - 32;
+		a++;
+	}
+	return (s);
+}
