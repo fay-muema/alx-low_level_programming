91412b42ee78360a77f8b9f6491da365487223ce
fay-muema <faithmuema45@gmail.com>
2022-06-17 12:13:16 -0700

    strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *k)
 {
 	int cou;
 	int a;
-	int words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(k + cou) >= 97 && *(k + cou) <= 122)
 		*(k + cou) = *(k + cou) - 32;
@@ -20,7 +20,7 @@ char *cap_string(char *k)
 	{
 		for (a = 0; a < 13; a++)
 		{
-			if (*(k + cou) == words[a])
+			if (*(k + cou) == sep_words[a])
 			{
 				if ((*(k + (cou + 1)) >= 97) && (*(k + (cou + 1)) <= 122))
 					*(k + (cou + 1)) = *(k + (cou + 1)) - 32;
