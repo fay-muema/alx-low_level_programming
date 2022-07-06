8629933680060996d1216457640734ecd01ea831
fay-muema <faithmuema45@gmail.com>
2022-07-05 22:30:43 -0700

    name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ *print_name_as_is - prints a name as is
+ *@name: name of the person
+ *Return: Alwaays returns 0
+ */
+
+void print_name_as_is(char *name)
+{
+	printf("hello, my name is %s\n", name);
+}
+
