1cdee082c26fb6d5a6c58f732ad72903a99fd1ba
fay-muema <faithmuema45@gmail.com>
2022-07-19 09:26:38 -0700

    function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ *yoo - prints the staement
+ * Return: returns nothing
+ */
+
+void yoo(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
