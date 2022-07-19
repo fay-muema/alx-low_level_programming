f837faad8fe6e08935bf0f433e3ffa308cef9fc5
fay-muema <faithmuema45@gmail.com>
2022-07-19 09:31:13 -0700

    function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,8 @@
  * Return: returns nothing
  */
 
-void yoo(void)
+void __attribute__ ((constructor)) yoo(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
